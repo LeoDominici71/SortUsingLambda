9e66e311339cf5e4cb8f95b9cffa0392ba3500fb
Leonardo De Paula Requena Dominici <leonardodominici@Leonardos-MacBook-Pro-350.local>
2022-12-26 01:08:32 -0300

    first commit

---
+++ src/programa/ProgramaComexpLamb.java
@@ -0,0 +1,31 @@
+package programa;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import entities.Product;
+
+public class ProgramaComexpLamb {
+
+	public static void main(String[] args) {
+		
+        List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+		//mesma coisa que a classe mycomparator porem no programa principal
+		
+		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+		
+		
+		
+		
+		for(Product p : list) {
+			System.out.println(p);
+		}
+
+	}
+
+}
